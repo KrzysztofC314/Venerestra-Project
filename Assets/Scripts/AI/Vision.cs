6eb11a408e5ac3c6756c729713e02ccd25cbdba6
KrzysztofC314 <115410290+KrzysztofC314@users.noreply.github.com>
2023-03-30 15:14:24 +0200

    vision update

--- Assets/Scripts/AI/Vision.cs
+++ Assets/Scripts/AI/Vision.cs
@@ -1,24 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class Vision : MonoBehaviour
 {
     const string Left = "left";
     const string Right = "right";
-    private float angle;
-    
 
+    private float angle;
     [SerializeField]
     private GameObject enemy;
     private AIController controller;
     [SerializeField]
     private Transform visionPosition;
     [SerializeField]
-    private float visionAngle;
+    private Transform playerPosition;
+
     [SerializeField]
-    private float visionRange;
+    private GameObject patrolLight;
+    [SerializeField]
+    private float patrolVisionAngle;
+    [SerializeField]
+    private float patrolVisionRange;
+
+    [SerializeField]
+    private GameObject chaseLight;
+    [SerializeField]
+    private float chaseVisionAngle;
+    [SerializeField]
+    private float chaseVisionRange;
     private string visionDirection;
+    private float visionAngle;
+    private float visionRange;
 
     [SerializeField]
     private Transform player;
@@ -33,8 +47,24 @@ public class Vision : MonoBehaviour
 
     private void FixedUpdate()
     {
+        switch (controller.state)
+        {
+            default:
+            case AIController.State.Patrol:
+                visionAngle = patrolVisionAngle;
+                visionRange = patrolVisionRange;
+                patrolLight.SetActive(true);
+                chaseLight.SetActive(false);
+                break;
+            case AIController.State.Chase:
+                visionAngle = chaseVisionAngle;
+                visionRange = chaseVisionRange;
+                patrolLight.SetActive(false);
+                chaseLight.SetActive(true);
+                break;
+        }
         visionDirection = controller.facingDirection;
-        Vector2 direction = player.position - transform.position;
+        Vector2 direction = playerPosition.position - transform.position;
         if (visionDirection == Right)
         {
             angle = Vector3.Angle(direction, visionPosition.right);
@@ -46,9 +76,9 @@ public class Vision : MonoBehaviour
         RaycastHit2D raycast = Physics2D.Raycast(visionPosition.position, direction, visionRange);
         if(angle < visionAngle / 2)
         {
+            Debug.DrawRay(visionPosition.position, direction, Color.blue);
             if (raycast.collider.CompareTag("Player")&& controller.state == AIController.State.Patrol)
             {
-                Debug.DrawRay(visionPosition.position, direction, Color.blue);
                 controller.speed = 0;
                 Invoke("Located", stunWait);
             }
